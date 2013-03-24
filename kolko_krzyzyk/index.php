07fed8ae2b653a647bc0b39b4269abbc682ee830
ravik122 <ravik1222@gmail.com>
2013-03-24 12:00:40 +0100

    started pairing players

--- kolko_krzyzyk/index.php
+++ kolko_krzyzyk/index.php
@@ -8,7 +8,20 @@
 <?php
 
 	$timestamp = date('d_h:i');
-	echo $timestamp;
-
+//	echo $timestamp;
+	$files = glob("*.wt");
+	if(empty($files))
+	{
+		$filename = $timestamp . ".wt";
+		touch($filename);
+	}
+	else
+	{
+		echo "there is another player!<br>";
+		$filename = $files[0];
+		$path =  substr($filename,0,-3);
+		unlink($filename);
+		mkdir("$path");
+	}
 ?>
 </html>
