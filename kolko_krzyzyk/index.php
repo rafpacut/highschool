842898dca3019ea315762b3166828eb6328c4438
ravik122 <ravik1222@gmail.com>
2013-03-26 14:48:57 +0100

    paired players and started their boards

--- kolko_krzyzyk/index.php
+++ kolko_krzyzyk/index.php
@@ -10,39 +10,50 @@
 	</form>
 
 </body>
-<?phP
+<?php
 if(isset($_GET['nick']))
 {
-	$f = fopen('players','r+');
-	if(flock($f, LOCK_EX))
-	{
-		$file = file_get_contents('players');
-		if(empty($file))
-		{
-			$timestamp = date('H:i:s');
-			fwrite($f,$timestamp);
-			fflush($f);
-			flock($f,LOCK_UN);
-			fclose($f);
-			while(!file_exists($timestamp))
-			{
-				sleep(3);
-			}
-			$filename = $timestamp . '/' . $nick;
-			touch($filename) or die("player1 nie zdolal stworzyc pliku");
-		}
-		else
-		{
-			ftruncate($f,0);
-			flock($f,LOCK_UN);
-			fclose($f);
-			$dirname = $file;
-			mkdir($file);
-			$filename = $file . '/' . $nick;
-			touch($filename)  or die("player2 nie zdolal stworzyc pliku");
-		}
-	}
+ 	$f = fopen('players','r+');
+ 	if(flock($f, LOCK_EX))
+ 	{
+ 		$file = file_get_contents('players');
+ 		if(empty($file))
+ 		{
+ 			$file =  date('H:i:s');
+ 			fwrite($f,$file);
+ 			fflush($f);
+ 			flock($f,LOCK_UN);
+ 			fclose($f);
+ 			while(!file_exists($file))
+ 			{
+ 				sleep(3);
+ 			}
+ 			$ready = true;
+ 		}
+ 		else
+ 		{
+ 			ftruncate($f,0);
+ 			flock($f,LOCK_UN);
+ 			fclose($f);
+ 			$dirname = $file;
+ 			mkdir($file);
+ 			$ready = true;
+ 		}
+ 	}
+ }
+ 
+ if(isset($ready) && isset($_GET['nick']))
+ {
+ 	$foo = file_get_contents('start_array.txt');
+ 	$dest=$file . '/' . $_GET['nick'];
+ 	copy('start_array.txt',$dest) or die('ehhh');
 }
+
+
+
+
+
+
 
 ?>
 </html>
