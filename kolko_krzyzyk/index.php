9737c1a18fb95906f8e602863407319bbcf7fc3b
$your_name <$your_email>
2013-04-01 14:03:51 +0200

    both players paired up

--- kolko_krzyzyk/index.php
+++ kolko_krzyzyk/index.php
@@ -1,59 +1,36 @@
 <html>
 <head>
+<?php
+if(!isset($_GET['player']))
+{
+	$directory='./';
+	$files = glob("*.wt");
+	if(empty($files))
+	{
+		echo "that's the first player!";
+		$time=date('H:i:s');
+		$channel = $time . '.wt';
+		$f = fopen($channel, "w+");
+		fclose($f);
+		chmod($channel,0664);
+		$player = 1;
+	}
+	else
+	{
+		echo "that's the second player!";
+		$channel = $files[0];
+		$player = 2;
+	}
+	echo '<Meta http-equiv="refresh" content="5;URL=http://localhost/kolko_krzyzyk/index.php?channel=' . $channel .  '&player=' . $player . '">';
+}
+else
+{
+	echo '<Meta http-equiv="refresh" content="5;URL=http://localhost/kolko_krzyzyk/index.php?channel=' . $_GET['channel'] .  '&player=' .  $_GET['player'] . '">';
+}
+
+?>
 </head>
 <body>
 
-	<form method=GET>
-		Podaj swoj nick:
-		<input type=text name=nick>
-		<input type=submit>
-	</form>
-
 </body>
-<?php
-if(isset($_GET['nick']))
-{
- 	$f = fopen('players','r+');
- 	if(flock($f, LOCK_EX))
- 	{
- 		$file = file_get_contents('players');
- 		if(empty($file))
- 		{
- 			$file =  date('H:i:s');
- 			fwrite($f,$file);
- 			fflush($f);
- 			flock($f,LOCK_UN);
- 			fclose($f);
- 			while(!file_exists($file))
- 			{
- 				sleep(3);
- 			}
- 			$ready = true;
- 		}
- 		else
- 		{
- 			ftruncate($f,0);
- 			flock($f,LOCK_UN);
- 			fclose($f);
- 			$dirname = $file;
- 			mkdir($file);
- 			$ready = true;
- 		}
- 	}
- }
- 
- if(isset($ready) && isset($_GET['nick']))
- {
- 	$foo = file_get_contents('start_array.txt');
- 	$dest=$file . '/' . $_GET['nick'];
- 	copy('start_array.txt',$dest) or die('ehhh');
-}
-
-
-
-
-
-
-
-?>
 </html>
