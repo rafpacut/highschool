0ee7dc5d67d904ef9ad4934eca37ba8c6589e1b6
$your_name <$your_email>
2013-04-01 14:50:48 +0200

    changed channel to contain info when player can make a move

--- kolko_krzyzyk/index.php
+++ kolko_krzyzyk/index.php
@@ -1,25 +1,30 @@
 <html>
 <head>
-<?php
-if(!isset($_GET['player']))
+<?php // parowanie graczy
+if(!isset($_GET['player'])) 
 {
-	$directory='./';
 	$files = glob("*.wt");
 	if(empty($files))
 	{
 		echo "that's the first player!";
-		$time=date('H:i:s');
-		$channel = $time . '.wt';
-		$f = fopen($channel, "w+");
+		$channel=date('H:i:s');
+		$filename = $channel . '.wt';
+		$f = fopen($filename, "w+");
 		fclose($f);
-		chmod($channel,0664);
+		chmod($filename,0664);
 		$player = 1;
+		$channel = $channel . '.' . $player;
 	}
 	else
 	{
 		echo "that's the second player!";
 		$channel = $files[0];
+		$channel = substr($channel, 0, -3);
 		$player = 2;
+		$newplayer = $player % 2 + 1;
+		$newfile = substr($files[0], 0, -3) . '.' . $newplayer;
+		rename($files[0], $newfile);
+		$channel = $channel . '.' . $player;
 	}
 	echo '<Meta http-equiv="refresh" content="5;URL=http://localhost/kolko_krzyzyk/index.php?channel=' . $channel .  '&player=' . $player . '">';
 }
@@ -31,6 +36,22 @@ else
 ?>
 </head>
 <body>
+
+<?php
+
+if(isset($_GET['channel']))
+{
+	$dh = opendir('./');
+	while(($file = readdir($dh)) !== false)
+	{
+		if($file == $_GET['channel'])
+		{
+			echo "twoj ruch";
+		}
+	}
+}
+
+?>
 
 </body>
 </html>
