c393fe86e0cd2372ad08b17bca93b1e7e6647891
ravik122 <ravik1222@gmail.com>
2013-05-18 21:38:40 +0200

    displaying required, readable results

--- dziennik/browse/grades.php
+++ dziennik/browse/grades.php
@@ -6,11 +6,21 @@
 mysql_connect('localhost', 'root', 'rootpassword');
 mysql_select_db('dziennik');
 
-$query = 'select * from main where id_osoba =' . $_GET['student'] ;
+$query = 'select * from main where id_osoba =' . $_GET['student'] . ' order by id_przedmiot';
 
 $db_struct = mysql_query( $query );
 
 while( $row = mysql_fetch_array( $db_struct ) )
 {
-	echo $row['id_przedmiot'] . ' ' . $row['id_ocena'] . '<br>';
+	$query_tmp = 'select nazwa from przedmioty where id =' . $row['id_przedmiot'];
+	$db_struct2 = mysql_query( $query_tmp );
+	$subject_name = mysql_fetch_array( $db_struct2 );
+	
+	echo '<br>'. $subject_name['nazwa'] . '<br>';	
+
+	$query_tmp = 'select nazwa from oceny where id =' . $row['id_ocena'];
+	$db_struct3 = mysql_query( $query_tmp );
+	$grade = mysql_fetch_array( $db_struct3 );
+
+	echo $grade['nazwa'] . ' ';
 }
