502ffbfa35d7320cbfd32e09ec02c22cba104917
ravik122 <ravik1222@gmail.com>
2013-07-02 18:09:35 +0200

    sending graph source and target data on user rightclick

--- googlemap/add_marker.js
+++ googlemap/add_marker.js
@@ -3,6 +3,8 @@ var markersArray = [];
 // if there is no markers in database, the first marker will have no id, 
 // and by clicking on the map the markersArray[0] would be NaN
 markersArray[0] = -1; 
+var graph_constructing = false;
+var json_str = [];
 
 
 function add_marker( location, marker_id )
@@ -22,10 +24,37 @@ function add_marker( location, marker_id )
 		marker.setMap( null );
 	});
 
+
 	//send marker position to database via php:
 	//w tej chwili unikalnosc markerow jest zachowana dzieki odrzucaniu duplikatow przez baze danych,
 	//nie lepiej byloby sprawdzic ja iterujac po tablicy markerow?
 	$.post("/googlemap/center.php", { 'marker_pos[]': [ marker_id, location.lat(), location.lng() ]});
+
+	google.maps.event.addListener( marker, 'rightclick', function()
+	{
+		if( graph_constructing )
+        	{
+			graph_constructing = false;
+
+			json_str[1] = [];
+			json_str[1][0] = marker_id;
+			json_str[1][1] = location.lat();
+			json_str[1][2] = location.lng();
+
+			$.post("/googlemap/shortest_paths/graph_init.php", JSON.stringify( json_str ) );
+		}
+		else
+		{
+			graph_constructing = true;
+
+			json_str[0] = [];
+			json_str[0][0] = marker_id;
+			json_str[0][1] = location.lat();
+			json_str[0][2] = location.lng();
+		}
+
+		
+	});
 
 	markersArray.push( marker_id );
 }
@@ -69,4 +98,6 @@ var distanceWidget = new DistanceWidget( map );
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
